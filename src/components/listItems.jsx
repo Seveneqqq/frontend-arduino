9bc1f96185f2a36c50c0a2f8bfa628bdbb0f73d0
Paweł Boroń <pawcio2001.boron@interia.pl>
2024-06-29 14:57:12 +0200

    Added react router to menu

--- src/components/listItems.jsx
+++ src/components/listItems.jsx
@@ -4,57 +4,58 @@ import database from "../icons/database.svg";
 import home from "../icons/home.svg";
 import login from "../icons/login.svg";
 import mic from "../icons/mic.svg";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 
 const items = [
     {
         text: "Home",
         img: home,
+        path: "/home"
     },
     {
         text: "Author",
         img: author,
+        path: "/author"
     },
     {
         text: "Speech Api",
         img: mic,
+        path: "/speech-api"
     },
     {
         text: "Stack",
         img: code,
+        path: "/stack"
     },
     {
         text: "Database",
         img: database,
+        path: "/database"
     },
     {
         text: "Login",
         img: login,
+        path: "/login"
     }
 ];
 
-function changePage(page){
-    let path = page.toLowerCase();
-    if(path === "speech api"){
-        path="speech-api";
-    }
-    window.location = `http://localhost:3000/${path}`;
-}
-
 //<img className="fill-white h-[48%]" src={item.img} alt={item.text}/>
+
 
 export default function ListItem() {
 
-    return(
-
+    return (
         <ul className="flex flex-row justify-between h-full w-full gap-6 items-center z-3">
-            {items.map(item => { 
-                return(
-                    <li className="flex flex-col items-center py-1 justify-center w-[6vw] h-[85%] rounded-[100px] hover:bg-[#35363C] active:bg-[#35363C] z-3" onClick={()=>changePage(item.text)}>
-                        <span className="text-[1vw] text-fuchsia-300">{item.text}</span>
-                    </li>
-            )})}
+            {items.map(item => {
+                return (
+                    <Link to={item.path} className="flex flex-col items-center text-[1vw] text-fuchsia-300 py-1 justify-center w-[6vw] h-[85%] rounded-[100px] hover:bg-[#35363C] active:bg-[#35363C] z-3">   
+                        <li key={item.path}>
+                            {item.text} 
+                        </li>
+                    </Link>
+                );
+            })}
         </ul>
-
-    )
+    );
 }
