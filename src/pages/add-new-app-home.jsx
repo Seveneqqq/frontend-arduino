31c6db40924820b98dd5a9cdef29ccec7b320c9a
Paweł Boroń <pawcio2001.boron@interia.pl>
2024-08-28 19:01:16 +0200

    dodanie toasta przy dolaczeniu do domu - przygotowanie do dolaczania

--- src/pages/add-new-app-home.jsx
+++ src/pages/add-new-app-home.jsx
@@ -4,14 +4,18 @@ import { StepperPanel } from 'primereact/stepperpanel';
 import { Button } from 'primereact/button';
 import { Panel } from '../components/panel';
 import { InputText } from "primereact/inputtext";
-
+import { Toast } from 'primereact/toast';
 
 export default function AddNewAppHome(){
 
     const stepperRef = useRef(null);
-    
+    const toast = useRef(null);
     const [isClicked,clickedButton] = useState(false);
     const [valueHomeName,setValueOfHomeName] = useState('');
+
+    const showSuccess = () => {
+        toast.current.show({severity:'success', summary: 'Success', detail:'Succesfully joined into house.',life: 111000,});
+    }
 
     useEffect(() => {
 
@@ -39,8 +43,27 @@ export default function AddNewAppHome(){
 
             let joinCode = document.getElementById('inviteCode').value;
             let userId = sessionStorage.getItem('UserId');
+            console.log('Bearer ' + sessionStorage.getItem('AuthToken'));
+            let response = await fetch('http://localhost:4000/api/join-to-home', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json', 
+                    'Authorization': 'Bearer ' + sessionStorage.getItem('AuthToken')
+                },
+                body: JSON.stringify({
+                    "user_id": userId,
+                    "home_invite_code": joinCode
+                })
+            });
 
-            let response = await fetch();
+            if( response.ok){
+                let data = await response.json();
+                showSuccess();
+            }
+            else{
+                //error
+            }
+
 
         } catch (error) {
             console.log(error);
@@ -55,7 +78,7 @@ export default function AddNewAppHome(){
                 <div className="flex flex-column h-[80vh]">
                         <div className="!bg-slate-800 surface-ground flex-auto flex justify-content-center align-items-center font-medium">
                             <div className="flex flex-col items-center w-[100%] gap-5 justify-center">
-
+                                <Toast ref={toast}/>
                                 
                                     {!isClicked ?
                                     <>
