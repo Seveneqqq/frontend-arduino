b5cae0e9c39a1d35d9ed252c818e9f7b6468e0eb
Paweł Boroń <pawcio2001.boron@interia.pl>
2024-08-29 18:23:21 +0200

    zapisywanie wartosci inputa z kodem

--- src/pages/add-new-app-home.jsx
+++ src/pages/add-new-app-home.jsx
@@ -14,6 +14,7 @@ export default function AddNewAppHome(){
     const toast = useRef(null);
     const [isClicked,clickedButton] = useState(false);
     const [valueHomeName,setValueOfHomeName] = useState('');
+    const [inviteCode, setInviteCode] = useState('');
 
     const showSuccess = () => {
         toast.current.show({severity:'success', summary: 'Success', detail:'Succesfully joined into house.',life: 2000,});
@@ -48,7 +49,7 @@ export default function AddNewAppHome(){
 
             let joinCode = document.getElementById('inviteCode').value;
             let userId = sessionStorage.getItem('UserId');
-            console.log('Bearer ' + sessionStorage.getItem('AuthToken'));
+            
             let response = await fetch('http://localhost:4000/api/join-to-home', {
                 method: 'POST',
                 headers: {
@@ -103,7 +104,7 @@ export default function AddNewAppHome(){
                                     : 
                                     <>
                                         <h2>Join to existing house</h2>
-                                            <InputText keyfilter="int" id="inviteCode" placeholder="#123456" maxLength={6}/>
+                                            <InputText keyfilter="int" value={inviteCode} id="inviteCode" placeholder="#123456" maxLength={6} onChange={(event) => setInviteCode(event.target.value)}/>
                                             <Button label="Join" icon="pi pi-plus" onClick={()=>joinToHouse()}/>
                                         <h2>Or</h2>
                                             <div className="flex flex-col gap-5">
