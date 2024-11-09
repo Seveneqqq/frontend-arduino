5740c90a748d6b70a1ba4d81d9e820a74188e30b
Paweł Boroń <pawcio2001.boron@interia.pl>
2024-11-09 16:59:20 +0100

    Dalszy rozwoj dashboard

--- src/pages/panel-dashboard.jsx
+++ src/pages/panel-dashboard.jsx
@@ -4,7 +4,7 @@ export default function PanelDashboard(){
 
     return(
         <div className="w-full bg-[#080808]">
-            <header className="flex xl:flex-row flex-col justify-between items-center mx-5 my-3 px-7 py-0 rounded-[100px] bg-[#151513]">
+            <header className="flex xl:flex-row flex-col justify-between items-center mx-5 mt-3 px-7 py-0 rounded-[100px] bg-[#151513]">
                 <ul className="flex gap-3 xl:flex-row flex-col">
                     <li className="rounded-[100px] text-[#080808] bg-[#C7EE7C] w-[150px] my-2 py-2 flex justify-center items-center text-xl">
                         <p>Dashboard</p>
@@ -35,17 +35,19 @@ export default function PanelDashboard(){
                 </ul>
             </header>
 
-            <div className="my-3 px-7 py-7 h-[90vh]">
+            <div className="my-0 px-7 py-7 h-[90vh] flex flex-col gap-5">
                 <div className="flex gap-5">
-                    <div className="w-full h-64 bg-slate-600 rounded-xl "></div>
-                    <div className="w-full h-64 bg-slate-600 rounded-xl "></div>
-                    <div className="w-full h-64 bg-slate-600 rounded-xl "></div>
-                    <div className="w-full h-64 bg-slate-600 rounded-xl "></div>
-                    <div className="w-full h-64 bg-slate-600 rounded-xl "></div>
+                    <div className="w-full h-48 bg-[#151513] rounded-xl "></div>
+                    <div className="w-full h-48 bg-[#151513] rounded-xl "></div>
+                    <div className="w-full h-48 bg-[#151513] rounded-xl "></div>
+                    <div className="w-full h-48 bg-[#151513] rounded-xl "></div>
+                    <div className="w-full h-48 bg-[#151513] rounded-xl "></div>
                 </div>
-                <div className="">
 
+                <div className="w-full h-[75%] bg-[#151513] rounded-xl">
+                    das
                 </div>
+                
             </div>
         </div>
     );
