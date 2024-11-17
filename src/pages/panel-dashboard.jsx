14d05863acd9dbe14815aa3a3d9e7422f1a5bcfc
Paweł Boroń <pawcio2001.boron@interia.pl>
2024-11-17 22:19:44 +0100

    Dodanie zarysu wyswietlania powiadomien

--- src/pages/panel-dashboard.jsx
+++ src/pages/panel-dashboard.jsx
@@ -1,16 +1,23 @@
 import { Badge } from 'primereact/badge';
 import { Avatar } from 'primereact/avatar';
 import { useState, useRef } from 'react';
+import { OverlayPanel } from 'primereact/overlaypanel';
+
+
 import FastAccesElement from '../components/fastAccessElement';
 
 export default function PanelDashboard() {
-    const [notifications, setNotifications] = useState([]);
+    
     const scrollRef = useRef(null);
+    const op = useRef(null);
+
+    const [notifications, setNotifications] = useState([]);
     const [isDragging, setIsDragging] = useState(false);
     const [startX, setStartX] = useState(0);
     const [scrollLeft, setScrollLeft] = useState(0);
     const [microphoneActivated, setMicrophoneActivated] = useState(false);
     const [microphoneActivatedColor, setMicrophoneActivatedColor] = useState('bg-[#080808]');
+    
 
     const handleMouseDown = (e) => {
         setIsDragging(true);
@@ -37,7 +44,7 @@ export default function PanelDashboard() {
 
     return (
         <div className="w-full bg-[#080808] min-h-screen flex flex-col">
-            <header className="flex xl:flex-row flex-col justify-between items-center mx-5 mt-3 px-7 py-0 rounded-[100px] bg-[#151513]">
+            <header className="flex xl:flex-row flex-col justify-between items-center mx-5 mt-3 px-7 py-0 xl:rounded-[100px] rounded-md bg-[#151513]">
                 <ul className="flex gap-3 xl:flex-row flex-col">
                     <li className="rounded-[100px] text-[#080808] bg-[#C7EE7C] w-[150px] my-2 py-2 flex justify-center items-center text-xl hover:cursor-pointer cursor-default">
                         <p>Dashboard</p>
@@ -59,14 +66,23 @@ export default function PanelDashboard() {
                     <li className={`${microphoneActivatedColor} rounded-[100%] p-2 w-[55px] h-[55px] flex items-center justify-center hover:cursor-pointer cursor-default transition ease-out delay-150 duration-500`} onClick={microphoneActivate}>
                         <i className='pi pi-microphone transition before:ease-out before:delay-150 before:duration-150 text-[1.5em]' />
                     </li>
-                    <li className="bg-[#080808] rounded-[100%] p-2 w-[55px] h-[55px] flex items-center justify-center">
+                    <li className="bg-[#080808] rounded-[100%] p-2 w-[55px] h-[55px] flex items-center justify-center" onClick={(e) => op.current.toggle(e)}>
                         <i className="pi pi-bell p-overlay-badge" style={{ fontSize: '1.5rem' }}>
-                            {notifications.length > 0 ? (
+                            {/* {notifications.length > 0 ? (
                                 <Badge 
                                     value={notifications.length} 
                                     className='bg-[#C7EE7C] !text-[#080808] flex justify-center items-center text-[0.8rem]' 
                                 />
-                            ) : ''}
+                            ) : ''} */}
+                             <OverlayPanel ref={op} className='bg-[#151513]'>
+                                <div classname="flex flex-col">
+                                    {notifications.length > 0 ? (
+                                        <span className='text-[1rem]'>Notification 1</span>
+                                    ) : (
+                                        <span className='text-[1rem]'>Nothing to show</span>
+                                    )}
+                                </div>
+                            </OverlayPanel>
                         </i>
                     </li>
                     <li className="bg-[#080808] rounded-[100%] w-[55px] h-[55px] flex items-center justify-center">
